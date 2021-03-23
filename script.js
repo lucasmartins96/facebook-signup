af47ab426023dd74b2508458f3340788ab5648bf
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-23 13:11:37 -0300

    script: adicionando a funçao resetRightContent() que remove os elementos filhos da classe rightContent e a funçao showUserData() que exibe os dados do formulario no lugar do formulario

--- script.js
+++ script.js
@@ -25,6 +25,27 @@ function isChecked(array) {
   return false;
 }
 
+function resetRightContent() {
+  const rightContent = document.querySelector('.right-content');
+  while (rightContent.firstChild) {
+    rightContent.removeChild(rightContent .firstChild);
+  }
+}
+
+function showUserData() {
+  const rightContent = document.querySelector('.right-content');
+  const inputs = document.querySelectorAll('.form-cadastro input');
+  const paragraphUserData = document.createElement('p');
+  paragraphUserData.innerText = 'Olá, ';
+  for (let index = 0; index < inputs.length; index += 1) {
+    if (inputs[index].type === 'text' || inputs[index].checked) {
+      paragraphUserData.innerText += `${inputs[index].value} `;
+    }
+  }
+  resetRightContent();
+  rightContent.appendChild(paragraphUserData);
+}
+
 function checkInputs(event) {
   event.preventDefault();
   const inpTex = document.querySelectorAll('.form-cadastro input[type="text"]');
@@ -35,6 +56,7 @@ function checkInputs(event) {
     span.innerText = 'Campos inválidos';
   } else {
     span.innerText = '';
+    showUserData();
   }
 }
 

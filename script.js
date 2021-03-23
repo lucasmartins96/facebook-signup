b23769ae8a749c9f0d602503b51be53fe7093110
DanielTrybe <darkdanielr@gmail.com>
2021-03-22 22:16:39 -0300

    18 completo

--- script.js
+++ script.js
@@ -7,24 +7,27 @@ function addEventButton() {
 }
 addEventButton();
 
-// function checkInputs(event) {
-//   event.preventDefault();
-//   const inputsTexts = document.querySelectorAll('.form-cadastro input[type="text"]');
-//   const inputPswd = document.querySelector('.form-cadastro input[type="password"]');
-//   const formCadastro = document.querySelector('.form-cadastro');
-//   const span = document.createElement('span');
-//   for (let index = 0; index < inputsTexts.length; index += 1) {
 
-//     if (inputsTexts[index].value === '' || inputPswd.value === '') {
+function isEmpty(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index].value === '') {
+      return true;
+    }
+  }
+  return false;
+}
 
-//       span.classList.add('span');
-//       formCadastro.appendChild(span).innerText = 'Campos inválidos';
-//     }
-//     if (document.getElementsByClassName('span')[1].classList.contains['span']) {
-//       document.getElementsByClassName('span')[1].removeChild("span")[1]
-//     }
-//   }
- 
-// }
-// const submitButton = document.getElementById('facebook-register');
-// submitButton.addEventListener('click', checkInputs);
+function checkInputs(event) {
+  event.preventDefault();
+  const inputsTexts = document.querySelectorAll('.form-cadastro input[type="text"]');
+  const inputPswd = document.querySelector('.form-cadastro input[type="password"]');
+  const span = document.querySelector('span');
+  if (isEmpty(inputsTexts) || inputPswd.value === '') {
+    span.innerText = 'Campos inválidos';
+  } else {
+    span.innerText = '';
+  }
+}
+
+const submitButton = document.getElementById('facebook-register');
+submitButton.addEventListener('click', checkInputs);
